33caf0f20a1d9e3345cd0e36ba3cad325f5e6cc4
isucon <isucon@isucon.com>
2024-07-09 14:38:47 +0000

    fix

--- sql/init.sh
+++ sql/init.sh
@@ -68,6 +68,6 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < initial_livecomments.sql
 
-ssh 192.168.0.13 -A 'bash ../pdns/init_zone.sh'
+ssh 192.168.0.13 -A 'bash webapp/pdns/init_zone.sh'
 
 

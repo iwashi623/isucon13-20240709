622c0fe6f5d98ef45e43e0ce23fef9d37c17c40b
iwashi623 <wonderland.7878@gmail.com>
2024-07-10 00:28:10 +0900

    fix

--- go/user_handler.go
+++ go/user_handler.go
@@ -256,7 +256,7 @@ func registerHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to insert user theme: "+err.Error())
 	}
 
-	if out, err := exec.Command("pdnsutil", "add-record", "t.isucon.pw", req.Name, "A", "0", powerDNSSubdomainAddress).CombinedOutput(); err != nil {
+	if out, err := exec.Command("ssh", "192.168.0.13", "-A", fmt.Sprintf("pdnsutil add-record t.isucon.pw %s A 0 192.168.0.11", req.Name)).CombinedOutput(); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, string(out)+": "+err.Error())
 	}
 
